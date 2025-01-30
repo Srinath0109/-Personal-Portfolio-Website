550e8a578b7cfb82679af4408d8f49121d9deb4a
Srinath Murugesan <162076816+Srinath0109@users.noreply.github.com>
2025-01-30 20:54:35 +0530

    Create About.js

---
+++ src/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+        <section id="about" className="about">
+            <h2>About Me</h2>
+            <p>I am a passionate software developer specializing in front-end and back-end development.</p>
+        </section>
+    );
+}
+
+export default About;
